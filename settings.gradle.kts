605a84f050bb227ac231060e9e31c7355df89ce2
MATHEUS ALVES REIS DA SILVA <22282211@dsn127m1247444.cfp127aluno.local>
2023-09-15 13:19:06 -0300

    subindo

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ds3t_api_livraria"
+include(":app")
